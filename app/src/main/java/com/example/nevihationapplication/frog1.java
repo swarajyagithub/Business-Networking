6bad69099f78d701eed606c60dd666187d71bcbe
Ashwini <ashwini.nikam7290@gmail.com>
2020-12-29 13:26:30 +0530

    BA

---
+++ app/src/main/java/com/example/nevihationapplication/frog1.java
@@ -0,0 +1,27 @@
+package com.example.nevihationapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link frog1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class frog1 extends Fragment {
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frog1, container, false);
+    }
+}
